7995f361fc641680b98c8458fe3a4265d547cf4b
Areeba Amjad <areeba.amjad2020@gmail.com>
2025-10-02 21:22:26 +0500

    Initial commit

---
+++ OnlineBookStore/Notes.cs
@@ -0,0 +1,11 @@
+﻿
+// ================================== 01: Identity Scaffolding ==============================
+/*
+    . When project is built first we will not see Identity pages like Login and Register to see them we need to scaffold them.
+    . Since we have configured the Individual Identity Account so it has configured alot of things for us e.g. DBContext class, Migrations folder etc
+    . We dont have to configure DBContext cuz it has did connectivity stuff by which we connet to DB.
+    . When u run the project u will see Identity/Account/Login when click on Login link but when u see the folder structure u will not see any Identity folder for this we have to do scafolding.
+    . Scaffolding is a way to quickly generate code for common application features, such as user authentication, CRUD operations, and more.
+    . 
+
+*/
